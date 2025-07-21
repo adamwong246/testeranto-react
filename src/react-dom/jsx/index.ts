b3b7a798e343311166d4a02dbed77d41c5c176d9
adam wong <adamwong246@gmail.com>
2025-07-20 23:33:30 -0700

    init

---
+++ src/react-dom/jsx/index.ts
@@ -0,0 +1,6 @@
+export type IInput = (props?) => JSX.Element;
+export type IState = unknown;
+export type ISelection = HTMLElement;
+export type IStore = HTMLElement;
+export type IWhenShape = any;
+export type IThenShape = any;
---
+++ testeranto.config.ts
@@ -0,0 +1,48 @@
+import { IProject } from "testeranto/src/Types";
+
+const config: IProject = {
+  projects: {
+    reactTests: {
+
+      src: "src",
+
+      debugger: false,
+      minify: false,
+      clearScreen: false,
+      externals: [],
+      ports: [],
+
+      featureIngestor: async function (s: string) {
+        return "";
+      },
+
+      tests: [
+        [
+          "./src/SubPackages/react-test-renderer/component/test/node.ts",
+          "node",
+          { ports: 0 },
+          [],
+        ],
+        [
+          "./src/SubPackages/react-test-renderer/component/test/web.ts",
+          "web",
+          { ports: 0 },
+          [],
+        ],
+        [
+          "./src/SubPackages/react-test-renderer/component/test/pure.ts",
+          "pure",
+          { ports: 0 },
+          [],
+        ],
+      ],
+
+      webPlugins: [],
+      nodePlugins: [],
+      importPlugins: [],
+      externalTests: {},
+      
+    },
+  },
+};
+export default config;
